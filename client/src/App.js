7fb8819451f7872102eaa44367b283e81b21cb29
Tommy Coleman <tommiilee@gmail.com>
2019-08-23 11:37:12 -0400

    added login page with authentication and private route to bubble page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import PrivateRoute from './components/routes/PrivateRoute';
 
 import Login from "./components/Login";
 import "./styles.scss";
+import BubblePage from "./components/BubblePage";
 
 function App() {
   const [colorList, setColorList] = useState([]);
@@ -10,6 +12,7 @@ function App() {
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
+        <PrivateRoute path='/bubbles' component={BubblePage} />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 
-const Login = () => {
+
+const Login = (props) => {
+  const [user, setUser] = useState({
+    username: '',
+    password: '',
+  })
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const userHandler = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setUser({...user, [name]: value})
+  }
+
+  const login = () => {
+    axios
+      .post('http://localhost:5000/api/login', user)
+      .then(res =>  {
+        console.log(res)
+        localStorage.setItem('token', res.data.payload)
+        props.history.push('/bubbles');
+      })
+      .catch(err => console.log(err))
+  }
+
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+     <form onSubmit={(e) => {
+       e.preventDefault()
+       login();
+     }}>
+       <input type='text' name='username' onChange={(e) => userHandler(e)} value={user.username} placeholder='Username' />
+       <input type='password' name='password' onChange={(e) => userHandler(e)} value={user.password} placeholder='Password' />
+       <button>Login</button>
+     </form>
     </>
   );
 };
